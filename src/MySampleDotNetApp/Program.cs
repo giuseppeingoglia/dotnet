ffa5028dcb02f91f1179a188c2936da7a678a6b1
Giuseppe Ingoglia <giuseppe.ingoglia@protonmail.com>
2024-09-12 16:43:23 +0100

    fixed the build

--- src/MySampleDotNetApp/Program.cs
+++ src/MySampleDotNetApp/Program.cs
@@ -1,15 +1,21 @@
+using System; 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// No OpenAPI/Swagger service registration
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// No OpenAPI/Swagger middleware configuration
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // No OpenAPI/Swagger mapping
 }
 
 app.UseHttpsRedirection();
@@ -21,10 +27,10 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            DateTime.Now.AddDays(index),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
         ))
@@ -35,7 +41,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
